14c6cd5bffbaee83f19ad351d3075a9d244b42a5
Paul Adams <amorie84@gmail.com>
2016-04-21 22:39:23 -0400

    Started Assignment5, need to add error handling

---
+++ Assignment05/Assignment05/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Assignment05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbInput.Location = new System.Drawing.Point(12, 12);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(275, 26);
+            this.tbInput.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(12, 61);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(35, 35);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "+";
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtract.Location = new System.Drawing.Point(60, 61);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(35, 35);
+            this.btnSubtract.TabIndex = 2;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiply.Location = new System.Drawing.Point(108, 61);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(35, 35);
+            this.btnMultiply.TabIndex = 3;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivide.Location = new System.Drawing.Point(156, 61);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(35, 35);
+            this.btnDivide.TabIndex = 4;
+            this.btnDivide.Text = "/";
+            this.btnDivide.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEquals.Location = new System.Drawing.Point(204, 61);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(35, 35);
+            this.btnEquals.TabIndex = 5;
+            this.btnEquals.Text = "=";
+            this.btnEquals.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(252, 61);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(35, 35);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "C";
+            this.btnClear.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnEquals;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 113);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Assignment05/Assignment05/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment05
+{
+    public partial class Form1 : Form
+    {
+        private decimal _input;
+        private decimal? _value;
+        private Opperands? _lastOpperator;
+        private bool _noError;
+
+        private enum Opperands
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _input = 0;
+            _value = null;
+            _lastOpperator = null;
+            _noError = true;
+            tbInput.Focus();
+        }
+
+        private void ClearAll()
+        {
+            _input = 0;
+            _value = null;
+            _lastOpperator = null;
+            tbInput.Clear();
+            tbInput.ForeColor = DefaultForeColor;
+            _noError = true;
+        }
+
+        private void ClearRecent()
+        {
+            _input = 0;
+            _lastOpperator = null;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ApplyLastOpperand();
+            _lastOpperator = Opperands.Add;
+            tbInput.Clear();
+            tbInput.Focus();
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            ApplyLastOpperand();
+            _lastOpperator = Opperands.Subtract;
+            tbInput.Clear();
+            tbInput.Focus();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            ApplyLastOpperand();
+            _lastOpperator = Opperands.Multiply;
+            tbInput.Clear();
+            tbInput.Focus();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            ApplyLastOpperand();
+            _lastOpperator = Opperands.Divide;
+            tbInput.Clear();
+            tbInput.Focus();
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            ApplyLastOpperand();
+            tbInput.Text = _value.ToString();
+            ClearRecent();
+            tbInput.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+            tbInput.Focus();
+        }
+
+        private bool IsValidInput()
+        {
+            decimal result;
+
+            if (decimal.TryParse(tbInput.Text, out result))
+            {
+                _input = result;
+                return true;
+            }
+
+            return false;
+        }
+
+
+        private void ApplyLastOpperand()
+        {
+            if (IsValidInput())
+            {
+                switch (_lastOpperator)
+                {
+                    case Opperands.Add:
+                        _value += _input;
+                        break;
+                    case Opperands.Subtract:
+                        _value -= _input;
+                        break;
+                    case Opperands.Multiply:
+                        _value *= _input;
+                        break;
+                    case Opperands.Divide:
+                        if (_input == 0)
+                        {
+                            tbInput.Text = "Cannot Divide by 0, Click Clear (C) to start over";
+                            tbInput.ForeColor = Color.Red;
+                            _noError = false;
+                            return;
+                        }
+                        _value /= _input;
+                        break;
+                    default:
+                        _value = _input;
+                        break;
+                }
+            }
+        }
+
+        private bool DividingByZero()
+        {
+            if (_lastOpperator == Opperands.Divide)
+            {
+                if (_input == 0)
+                {
+                    _noError = false;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
